61d9c37adc6d603cb9dc548f66963236449623fd
karan8904 <karanchavda210@gmail.com>
2025-06-19 15:46:06 +0530

    Created update api for product

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,8 +5,8 @@ import fs from "fs";
 export const addProduct = async (req, res) => {
   try {
     const { name, description, category, price, sizes, colors } = req.body;
-    console.log(req.body)
-    console.log(req.file)
+    console.log(req.body);
+    console.log(req.file);
     const isProductExists = await Product.findOne({
       name: { $regex: `^${name}$`, $options: "i" },
       category: category,
@@ -34,22 +34,54 @@ export const addProduct = async (req, res) => {
 };
 
 export const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find().populate('category', 'name')
-        res.status(200).json({ products }) 
-    } catch (error) {
-        res.status(400).json({ message: "Cannot fetch the products. Refresh the page..." })
-    }
-}
+  try {
+    const products = await Product.find().populate("category", "name");
+    res.status(200).json({ products });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Cannot fetch the products. Refresh the page..." });
+  }
+};
 
 export const deleteProduct = async (req, res) => {
-    try {
-        const id = req.params.id
-        const product = await Product.findByIdAndDelete(id)
-        product.images.forEach((image) => fs.unlinkSync(image))
-        res.status(200).json({ message: "Product deleted successfully.", product})
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({ message: "Cannot delete the product."})
+  try {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
+    product.images.forEach((image) => fs.unlinkSync(image));
+    res.status(200).json({ message: "Product deleted successfully.", product });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Cannot delete the product." });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { name, description, sizes, price, colors, category, images } = req.body;
+    const newData = {
+      name: name,
+      description: description,
+      sizes: sizes,
+      price: price,
+      category: category,
+      colors: colors,
+      images: []
+    };
+    if(req.files){
+      const files = req.files.map((img) => img.path )
+      newData.images = [...newData.images, ...files]
     }
-}
+    if(images){
+      newData.images = [...newData.images, ...images]
+    }
+
+    await Product.findByIdAndUpdate(id, newData)
+    res.status(200).json({ message: "Product updated succesully." });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Cannot update the product. Try again..." });
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { upload } from '../utils/multerConfig.js'
-import { addProduct, deleteProduct, getProducts } from '../controllers/productController.js'
+import { addProduct, deleteProduct, getProducts, updateProduct } from '../controllers/productController.js'
 
 export const productRoutes = express.Router()
 
 productRoutes.get('/', getProducts)
 productRoutes.post('/add', upload.array('images', 5), addProduct)
 productRoutes.delete('/delete/:id', deleteProduct)
+productRoutes.put('/edit/:id', upload.array('images', 5), updateProduct)
