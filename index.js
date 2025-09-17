5ae03d22f9a2cf790d1f7ec38f11cf66329fdf08
karan <karanchavda210@gmail.com>
2025-09-17 23:54:37 +0530

    changed index.js

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ import { dbConnection } from './config.js'
 import cors from 'cors'
 import { routes } from './routes/routes.js';
 import { offerEmails, subscriptionEmails } from './utils/cronJobs.js';
+import dotenv from 'dotenv';
 
 const app = express()
+dotenv.config()
 
 app.use(express.json())
 app.use(cors()) 
 app.use('/uploads', express.static('uploads'))
 app.use('/', routes)
 
-
+const PORT = process.env.PORT || 5000
 const startApp = async () => {
     try{
         await dbConnection()
-        app.listen(5000, () => {
-            console.log("App is connected with database and running on http://localhost:5000")
+        app.listen(PORT, () => {
+            console.log(`App is connected with database and running on http://localhost:${PORT}`)
         })
     }
     catch(error){
